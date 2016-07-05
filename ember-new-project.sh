8187c473e632b71fdceb583bda9c1bd0562806c0
Christian Jonassen <flyrev@gmail.com>
2016-07-05 11:37:05 +0200

    Extract method

--- ember-new-project.sh
+++ ember-new-project.sh
@@ -3,15 +3,19 @@
 set -e
 set -x
 
+install_addon() {
+    ember install $1
+    git add .
+    git commit -am "ember install ${addon}"
+}
+
 mkdir $1
 cd $1
 ember init
 
 for addon in ember-cli-template-lint ember-truth-helpers
 do
-    ember install ${addon}
-    git add .
-    git commit -am "ember install ${addon}"
+    install_addon ${addon}
 done
 
 npm install
